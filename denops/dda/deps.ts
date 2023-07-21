205c006dd32e1c5ed2578fad9de039d33dd6f5ee
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-07-21 12:13:19 +0900

    Align

--- denops/dda/deps.ts
+++ denops/dda/deps.ts
@@ -1,30 +1,30 @@
-export type { Denops } from "https://deno.land/x/denops_std@v4.1.6/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v4.1.6/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v4.1.6/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v4.1.6/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v4.1.6/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v4.1.6/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v4.1.6/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
   ensureObject,
   ensureString,
-} from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
+} from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.185.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.185.0/path/mod.ts";
-export { Lock } from "https://deno.land/x/async@v1.2.0/mod.ts";
+} from "https://deno.land/std@0.195.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.195.0/path/mod.ts";
+export { Lock } from "https://deno.land/x/async@v2.0.2/mod.ts";
 export {
   basename,
   dirname,
   sep as pathsep,
-} from "https://deno.land/std@0.185.0/path/mod.ts";
+} from "https://deno.land/std@0.195.0/path/mod.ts";
